af80e2a14ea8c67a6d971301f85206a0d41b96ca
tomoyukim <tomoyukim44@gmail.com>
2019-05-01 23:21:12 +0900

    introduce mermaid.cli and bind it with test command

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,52 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as cp from "child_process";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "mermaid-editor" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "mermaid-editor" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "extension.mermaidgen",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) return;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+      var filename = editor.document.uri.fsPath;
+      vscode.window.showInformationMessage(editor.document.uri.toString());
+      var workspaceFolder = vscode.workspace.getWorkspaceFolder(
+        editor.document.uri
+      );
+      var cwd = workspaceFolder ? workspaceFolder.uri.fsPath : "/";
+      vscode.window.showInformationMessage(cwd);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+      var command = context.extensionPath + "/node_modules/.bin/mmdc -t forest";
 
-	context.subscriptions.push(disposable);
+      cp.exec(
+        `${command} -i ${filename} -o sample.svg`,
+        { cwd },
+        (err, stdout, stderr) => {
+          if (err) {
+            vscode.window.showInformationMessage(stderr);
+          }
+          vscode.window.showInformationMessage(stdout);
+        }
+      );
+      vscode.window.showInformationMessage("Hello World!");
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
