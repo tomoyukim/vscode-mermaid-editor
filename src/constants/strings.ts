2b1c52a2845fb0b2b0affe8127a1d7509920a01f
Tomoyuki Murakami <tomoyukim@outlook.com>
2020-10-12 22:58:51 +0900

    fix wrong string for image generation

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,5 +1,5 @@
 export const STATUSBAR_MESSAGE_GENERATE_IMAGE = '$(sync) generating image...';
 
-export const MESSAGE_GENERATE_IMAGE_SUCCESS =
+export const MESSAGE_GENERATE_IMAGE_FAILURE =
   'mermaid-editor: Failed to generate image.';
-export const MESSAGE_GENERATE_IMAGE_FAILURE = `mermaid-editor: Succeeded to generate image!`;
+export const MESSAGE_GENERATE_IMAGE_SUCCESS = `mermaid-editor: Succeeded to generate image!`;
